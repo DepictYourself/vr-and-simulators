fa8d607042aa4e5c7427d622e6d566dfb5be45c7
DepictYourself <totha014@gmail.com>
2021-10-22 23:10:36 +0200

    Init orchard core

---
+++ src/Modules/VAS.Module/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "VAS.Module",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "VAS.Module",
+    Category = "Content Management"
+)]
